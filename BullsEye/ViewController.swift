798fd4d87748eb7a011f1cce8decde1eab35ab7f
Henngel Mojica <henngelm@icloud.com>
2018-02-04 13:39:55 -0300

    set up Outlets

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    @IBOutlet weak var slider: UISlider!
     var currentValue: Int = 0
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        currentValue = lroundf(slider.value)
     }
 
     override func didReceiveMemoryWarning() {
