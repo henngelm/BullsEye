fe9afd4bfd0dd9dad78af3e8b4f7765eb11b58ef
Henngel Mojica <henngelm@icloud.com>
2018-02-06 22:26:34 -0300

    the new look chapter 7

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -23,12 +23,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         startNewGame()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        let thumbImageNormal = #imageLiteral(resourceName: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = #imageLiteral(resourceName: "SliderTrackRight")
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+    
+    } 
     
     @IBAction func showAlert() {
         // calculate the difference between the random number and the slider's value.
@@ -53,7 +64,7 @@ class ViewController: UIViewController {
         } else {
             alertTitle = "Not even close!"
         }
-        print(difference)
+//        print(difference)
         // update the total score
         score += points
         
